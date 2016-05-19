bfdd2998bd886dff196c3f9654ed8529e00cc9ed
Kevin <findhope.china@gmail.com>
2016-05-19 17:15:55 +0800

    初始化项目

---
+++ BSBDJ/BSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ
+//
+//  Created by MitnickKevin on 16/5/19.
+//  Copyright © 2016年 SWUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
